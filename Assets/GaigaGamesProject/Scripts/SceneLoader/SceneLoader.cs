4189ac70a42b22c627beeecf700af1eec83caf01
Franciscorp <franciscorp1994@hotmail.com>
2024-05-03 17:17:57 +0100

    Loading Screen
    [Loading Screen] Use o minimum timer to apply
    [Utils] Constants are going to be used here

--- Assets/GaigaGamesProject/Scripts/SceneLoader/SceneLoader.cs
+++ Assets/GaigaGamesProject/Scripts/SceneLoader/SceneLoader.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections;
+using System.Runtime.ExceptionServices;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.Diagnostics;
 using UnityEngine.SceneManagement;
 using UnityEngine.WSA;
 
 public static class SceneLoader
 {
+
+    //Courotines need an instance that is MonoBehaviour, so we need to simulate it, a dummy object
+    private class LoadingMonoBehaviour : MonoBehaviour { }
 
     public enum Scene
     {
@@ -21,21 +27,37 @@ public static class SceneLoader
 
     private static Action onLoaderCallback;
 
-    private static IEnumerator LoadindScreenMininumTimeOnScreen()
+    private static IEnumerator LoadSceneAsync(Scene scene)
     {
-        Debug.Log("Waiting 1 second");
-        yield return new WaitForSecondsRealtime(1);
-        Debug.Log("DONE");
+        // Waits a mininum time
+        yield return new WaitForSecondsRealtime(Utils.LoadingScreenMinimumTime);
+        //yield return null;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene.ToString());
+
+
+        while (!asyncOperation.isDone)
+        {
+            //Waits for next frame until it is loaded
+            yield return null;
+        }
+
     }
 
     public static void Load(Scene scene)
     {
+        // TODO there is a small delay caused by the async opetation
+
         // Associates the calling of the loadscene inside the delegate action
         // Set the loader callback action to load the target scene
         // here is where it is preloaded the real scene, while it show cases the loading
         onLoaderCallback = () =>
         {
-            SceneManager.LoadScene(scene.ToString());
+            // Uses the private function to call the scene async
+            //First needs to start a courotine, which is ReadOnly called by a monobehaviour class
+            GameObject loadingGameObject = new GameObject("Loading Game Object");
+            loadingGameObject.AddComponent<LoadingMonoBehaviour>().StartCoroutine(LoadSceneAsync(scene));
+            LoadSceneAsync(scene);
         };
 
         // Load the loading screen
---
+++ Assets/GaigaGamesProject/Utils/Utils.cs
@@ -0,0 +1,11 @@
+public class Utils
+{
+    // SAVES FILE NAMES 
+    //public const string SaveDirectory = "Saves";
+    public const string SettingsSaveFilePath = "SettingsSave";
+    //public const string GameDataSaveFilePath = SaveDirectory + "/" + "GameDataSaveFilePath";
+
+    // TIMERS CONSTANTS
+    public const float LoadingScreenMinimumTime = 0.5f;
+
+}
